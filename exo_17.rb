74a510939e182ab8d922004690e2f66421caf147
nellotte <nelly.guerin@gmail.com>
2023-10-03 20:38:17 +0200

    modification exo 17

--- exo_17.rb
+++ exo_17.rb
@@ -11,11 +11,13 @@ if steps_number >25
 
 else
   puts "Voici la pyramide :" 
-  #structure de la pyramide : (etage-1)*epace + (etage-(etage-1))*# + (etage-1)*epace
+  #structure de la pyramide : (etage-1)*espace + (etage-(etage-1))*# + (etage-1)*espace
   1.upto(steps_number)do |n|
 
-  hashtag=(steps_number-(steps_number-1)) #calcul du nombre de # à afficher
-  space= (steps_number-1) #calcul du nombre d'espaces à afficher
+  hashtag= n*2-1  #calcul du nombre de # à afficher (2 fois le numéro de la ligne - 1)
+  #ex, ligne 1 il y a 1# soit 2*2-1, ligne 3 il y a 5# soit 2*3-1 etc...
+
+  space= (steps_number-n) #calcul du nombre d'espaces à afficher. C'est la différence entre le nombre d'étages et l'étage en cours.
 
   #Afficher les espaces " "  et afficher les étoiles
   puts " " * space + "#" * hashtag + " " * space
